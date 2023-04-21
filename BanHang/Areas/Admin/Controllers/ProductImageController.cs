1d7e45f83afdeaa2f7ac475516171c3203d50cca
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2023-04-21 16:15:18 +0700

    Fix ProductImage

--- BanHang/Areas/Admin/Controllers/ProductImageController.cs
+++ BanHang/Areas/Admin/Controllers/ProductImageController.cs
@@ -2,6 +2,7 @@
 using BanHang.Models.EF;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,9 +37,40 @@ namespace BanHang.Areas.Admin.Controllers
         public ActionResult Delete(int id)
         {
             var item = dbConect.ProductImages.Find(id);
-            dbConect.ProductImages.Remove(item);
+            var L_I_Counter = dbConect.ProductImages.Count();
+            if(L_I_Counter > 1 )
+            {
+                if(item.IsDefault)
+                {
+                    dbConect.ProductImages.First(x => x.Id != id).IsDefault = true; ;
+                    dbConect.ProductImages.Remove(item);
+                    dbConect.SaveChanges();
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    dbConect.ProductImages.Remove(item);
+                    dbConect.SaveChanges();
+                    return Json(new { success = true });
+                }
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult ChangeDefaultState(int Id)
+        {
+            var PrevDefaultState = dbConect.ProductImages.First(x => x.IsDefault);
+            var item = dbConect.ProductImages.Find(Id);
+            PrevDefaultState.IsDefault = false;
+            item.IsDefault = true;
+            dbConect.ProductImages.AddOrUpdate(item);
+            dbConect.ProductImages.AddOrUpdate(PrevDefaultState);
             dbConect.SaveChanges();
-            return Json(new { success = true });
+            return Json(new { Success = true });
         }
     }
 }
