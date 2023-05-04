95811eeb2410696ee94df78c33fadf58aea71e77
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-05-04 14:14:04 +0700

    New

--- BanHang/App_Start/RouteConfig.cs
+++ BanHang/App_Start/RouteConfig.cs
@@ -18,7 +18,18 @@ namespace BanHang
              defaults: new { controller = "Products", action = "Index", alias = UrlParameter.Optional },
              namespaces: new[] { "BanHang.Controllers" }
                 );
-
+            routes.MapRoute(
+            name: "NewsList",
+            url: "tin-tuc",
+            defaults: new { controller = "News", action = "Index", alias = UrlParameter.Optional },
+            namespaces: new[] { "BanHang.Controllers" }
+        );
+            routes.MapRoute(
+         name: "DetailNew",
+         url: "{alias}-n{id}",
+         defaults: new { controller = "News", action = "Detail", id = UrlParameter.Optional },
+         namespaces: new[] { "BanHang.Controllers" }
+     );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
