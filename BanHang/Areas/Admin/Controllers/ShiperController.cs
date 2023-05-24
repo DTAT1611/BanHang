b7e9f56cb5acde93bf7dd9dcfcc9d82b2e124949
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-05-24 14:41:00 +0700

    shiper

---
+++ BanHang/Areas/Admin/Controllers/ShiperController.cs
@@ -0,0 +1,33 @@
+﻿using BanHang.Models;
+using BanHang.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHang.Areas.Admin.Controllers
+{
+    public class ShiperController : Controller
+    {
+        // GET: Admin/Shiper
+        private ApplicationDbContext dbConect = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var items = dbConect.Shippers;
+            return View(items);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Shipper shipper)
+        {
+            dbConect.Shippers.Add(shipper);
+            dbConect.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BanHang/Models/EF/Shipper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BanHang.Models.EF
+{
+
+    [Table("tb_Shipper")]
+    public class Shipper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+    }
+}
