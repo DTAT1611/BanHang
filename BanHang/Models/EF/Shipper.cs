bc241b933801b2e8cfae16b631f17567db3ff778
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-05-28 14:45:34 +0700

    fix

---
+++ BanHang/Models/EF/Shipper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanHang.Models.EF
+{
+    [Table("tb_Shipper")]
+    public class Shipper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+    }
+}
