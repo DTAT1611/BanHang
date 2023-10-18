6324bb4a22fea04636ab89304383463a9146d94f
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2023-10-18 18:54:54 +0700

    Revert "Merge branch 'master' of https://github.com/DTAT1611/BanHang into old-master"
    This reverts commit 57cbdf548033b79c89c9c26e4a81b9cdee42b316, reversing
    changes made to 7c8d198c0f6e056b5f2c6060a5955e2384e9a475.

--- BanHang/Models/IdentityModels.cs
+++ BanHang/Models/IdentityModels.cs
@@ -17,9 +17,7 @@ namespace BanHang.Models
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-
             // Add custom user claims here
-            userIdentity.AddClaim(new Claim("Ava", this.Ava));
             return userIdentity;
         }
     }
