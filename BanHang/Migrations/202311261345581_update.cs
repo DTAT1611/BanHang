17bf9c71c7422d84233bec74fa121b6c1a0f7a44
DTAT1611 <113090251+DTAT1611@users.noreply.github.com>
2023-12-11 03:44:26 +0700

    cmt

---
+++ BanHang/Migrations/202311261345581_update.cs
@@ -0,0 +1,33 @@
+﻿namespace BanHang.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Sales",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        codesale = c.String(),
+                        productid = c.Int(nullable: false),
+                        percent = c.Int(nullable: false),
+                        userid = c.String(),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifierDate = c.DateTime(nullable: false),
+                        ModifierBy = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Sales");
+        }
+    }
+}
